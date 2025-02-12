060be10e354fa9def4f7ee228e6e49c4f5f42a53
jorted-runner <jorted.python@gmail.com>
2025-02-12 15:58:25 -0600

    Initial Commit

---
+++ CooksCabinet/CooksCabinetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooksCabinetApp.swift
+//  CooksCabinet
+//
+//  Created by Danny Ellis on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CooksCabinetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
